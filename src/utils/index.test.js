794df6660a515364e0e93c3008b911157d6f93c8
yafizh <a.chairil.yafizh@gmail.com>
2026-09-04 18:51:15 +0700

    deug

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -39,7 +39,7 @@ describe('stripHtml', () => {
   it('harus menghapus tag HTML dan merapikan spasi berlebih', () => {
     const html = '<p>Halo&nbsp;<strong>dunia</strong></p>   <p>React</p>';
 
-    expect(stripHtml(html)).toBe('Halo dunia React error');
+    expect(stripHtml(html)).toBe('Halo dunia React error error');
   });
 
   it('harus mengembalikan string kosong tanpa argumen', () => {
